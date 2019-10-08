d680c824c71ef4a9fb1acf381f9a22d3cbcfc67b
Ethan Cheatham (Tata Consultancy Services Ltd) <v-etchea@microsoft.com>
2019-10-08 11:19:00 -0700

    added data type validation

--- lib/app.ts
+++ lib/app.ts
@@ -2,15 +2,17 @@ import * as bodyParser from "body-parser";
 import * as express from "express";
 import * as mongoose from "mongoose";
 import Controller from "./interfaces/controller";
-
+import errorMiddleware from "./middleware/error.middleware";
 class App {
   public app: express.Application;
 
   constructor(controllers: Controller[]) {
     this.app = express();
+
     this.connectToTheDatabase();
     this.initializeMiddlewares();
     this.initializeControllers(controllers);
+    this.initializeErrorHandling();
   }
 
   public listen() {
@@ -21,6 +23,10 @@ class App {
 
   private initializeMiddlewares() {
     this.app.use(bodyParser.json());
+  }
+
+  private initializeErrorHandling() {
+    this.app.use(errorMiddleware);
   }
 
   private initializeControllers(controllers: Controller[]) {
--- lib/posts/posts.controller.ts
+++ lib/posts/posts.controller.ts
@@ -2,7 +2,9 @@ import * as express from "express";
 import Controller from "../interfaces/controller";
 import Post from "./post.interface";
 import postModel from "./post.model";
-
+import validationMiddleware from "../middleware/validation.middleware";
+import CreatePostDto from "./post.dto";
+import PostNotFoundException from "../exceptions/PostNotFoundException";
 class PostsController implements Controller {
   public path = "/posts";
   public router = express.Router();
@@ -17,7 +19,16 @@ class PostsController implements Controller {
     this.router.get(`${this.path}/:id`, this.getPostById);
     this.router.put(`${this.path}/:id`, this.modifyPost);
     this.router.delete(`${this.path}/:id`, this.deletePost);
-    this.router.post(this.path, this.createPost);
+    this.router.patch(
+      `${this.path}/:id`,
+      validationMiddleware(CreatePostDto, true),
+      this.modifyPost
+    );
+    this.router.post(
+      this.path,
+      validationMiddleware(CreatePostDto),
+      this.createPost
+    );
   }
 
   private getAllPosts = (
@@ -31,22 +42,47 @@ class PostsController implements Controller {
 
   private getPostById = (
     request: express.Request,
-    response: express.Response
+    response: express.Response,
+    next: express.NextFunction
   ) => {
     const id = request.params.id;
     this.post.findById(id).then(post => {
-      response.send(post);
+      if (post) {
+        response.send(post);
+      } else {
+        next(new PostNotFoundException(id));
+      }
     });
   };
 
   private modifyPost = (
     request: express.Request,
-    response: express.Response
+    response: express.Response,
+    next: express.NextFunction
   ) => {
     const id = request.params.id;
     const postData: Post = request.body;
     this.post.findByIdAndUpdate(id, postData, { new: true }).then(post => {
-      response.send(post);
+      if (post) {
+        response.send(post);
+      } else {
+        next(new PostNotFoundException(id));
+      }
+    });
+  };
+
+  private deletePost = (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction
+  ) => {
+    const id = request.params.id;
+    this.post.findByIdAndDelete(id).then(successResponse => {
+      if (successResponse) {
+        response.send(200);
+      } else {
+        next(new PostNotFoundException(id));
+      }
     });
   };
 
@@ -58,20 +94,6 @@ class PostsController implements Controller {
     const createdPost = new this.post(postData);
     createdPost.save().then(savedPost => {
       response.send(savedPost);
-    });
-  };
-
-  private deletePost = (
-    request: express.Request,
-    response: express.Response
-  ) => {
-    const id = request.params.id;
-    this.post.findByIdAndDelete(id).then(successResponse => {
-      if (successResponse) {
-        response.send(200);
-      } else {
-        response.send(404);
-      }
     });
   };
 }
